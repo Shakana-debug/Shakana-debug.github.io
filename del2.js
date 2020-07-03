c0ffa27ce2b1708b5a7d39c09ab87969fe042016
Shakana Jamal <65122325+Shakana-debug@users.noreply.github.com>
2020-07-03 12:17:49 +0200

    Add files via upload
    initial commit

---
+++ del2.js
@@ -0,0 +1,25 @@
+const slideNav = () => {
+    const humburger = document.querySelector('.hamburger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks =  document.querySelectorAll('.nav-links li');
+    
+    humburger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        
+        navLinks.forEach((link, index) => {
+       if( link.style.animation){
+            link.style.animation = '';
+       } else{
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`
+       }
+       
+    });
+        
+        hamburger.classList.toggle('toggle');
+        
+    });
+    
+    
+}
+                  
+ slideNav();                 
